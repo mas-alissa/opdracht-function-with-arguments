8f877bfcbffb5d6390cb9eddfa6c68d51ba0062d
mas-alissa <89016425+mas-alissa@users.noreply.github.com>
2021-09-26 15:18:59 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+// Single argument
+const paintingColors = function(color){
+    console.log("The wall has been painted " + color)
+    }
+    
+    paintingColors("green");
+    paintingColors("yellow");
+
+
+*/
+
+    // Multiple arguments
+
+    const paintingColors = function(place,color){
+        console.log("The "+ place +" wall has been painted " + color)
+        }
+        
+        paintingColors("north","orange");
+        paintingColors("south-east","gray");
